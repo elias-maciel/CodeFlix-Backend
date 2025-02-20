01dd70253053fd0ff2164e53b462bfd995d495f8
elias-maciel <eliasfsilvamaciel@gmail.com>
2025-02-20 17:18:01 -0300

    feat: add in_memory_repository

--- src/core/category/application/create_category.py
+++ src/core/category/application/create_category.py
@@ -1,15 +1,24 @@
 from uuid import UUID
 
 from src.core.category.domain.category import Category
+from src.core.category.infra.in_memory_category_repository import (
+    InMemoryCategoryRepository,
+)
 
 
 class InvalidCategoryData(Exception):
     pass
 
 
-def create_category(name: str, description: str = "", is_active: bool = True) -> UUID:
+def create_category(
+    repository: InMemoryCategoryRepository,
+    name: str,
+    description: str = "",
+    is_active: bool = True,
+) -> UUID:
     try:
         category = Category(name=name, description=description, is_active=is_active)
     except ValueError as e:
         raise InvalidCategoryData(e)
+    repository.save(category)
     return category.id
--- src/core/category/tests/application/test_create_category.py
+++ src/core/category/tests/application/test_create_category.py
@@ -1,20 +1,32 @@
+from unittest.mock import MagicMock
 from uuid import UUID
 
 import pytest
 
-from src.core.category.application.create_category import create_category, InvalidCategoryData
+from src.core.category.application.create_category import (
+    InvalidCategoryData,
+    create_category,
+)
+from src.core.category.infra.in_memory_category_repository import (
+    InMemoryCategoryRepository,
+)
 
 
 class TestCreateCategory:
     def test_create_category_with_valid_data(self):
+        mock_repository = MagicMock(spec=InMemoryCategoryRepository)
         category_id = create_category(
-            name="Movie", description="Description", is_active=True
+            repository=mock_repository,
+            name="Movie",
+            description="Description",
+            is_active=True,
         )
 
         assert category_id is not None
         assert isinstance(category_id, UUID)
+        assert mock_repository.save.called
 
     def test_create_category_with_invalid_data(self):
         with pytest.raises(InvalidCategoryData, match="name cannot be empty"):
-             create_category(name="")
-
+            mock_repository = MagicMock(spec=InMemoryCategoryRepository)
+            create_category(repository=mock_repository, name="")
