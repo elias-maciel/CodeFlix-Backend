6c6977f5282c7a8e072c87aa54004452b120b4ed
elias-maciel <eliasfsilvamaciel@gmail.com>
2025-02-20 17:32:45 -0300

    misc: lint

--- src/core/category/application/create_category.py
+++ src/core/category/application/create_category.py
@@ -7,21 +7,24 @@ from src.core.category.infra.in_memory_category_repository import (
     InMemoryCategoryRepository,
 )
 
+
 @dataclass
 class CreateCategoryRequest:
     name: str
     description: str = ""
     is_active: bool = True
 
+
 @dataclass
 class CreateCategoryResponse:
     id: UUID
+
 
 class CreateCategory:
     def __init__(self, repository: InMemoryCategoryRepository):
         self.repository = repository
 
-    def execute(self,request: CreateCategoryRequest) -> CreateCategoryResponse:
+    def execute(self, request: CreateCategoryRequest) -> CreateCategoryResponse:
         try:
             category = Category(
                 name=request.name,
