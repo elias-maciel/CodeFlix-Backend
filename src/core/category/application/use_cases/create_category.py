659a4b5d2bdedc0dcd06571c1a3c052f2278f40d
elias-maciel <eliasfsilvamaciel@gmail.com>
2025-02-20 18:19:04 -0300

    refact: change folders structure

--- src/core/category/application/use_cases/create_category.py
+++ src/core/category/application/use_cases/create_category.py
@@ -4,7 +4,7 @@ from uuid import UUID
 from src.core.category.application.category_repository import (
     CategoryRepository,
 )
-from src.core.category.application.exceptions import InvalidCategoryData
+from src.core.category.application.use_cases.exceptions import InvalidCategoryData
 from src.core.category.domain.category import Category
 
 
--- src/core/category/infra/in_memory_category_repository.py
+++ src/core/category/infra/in_memory_category_repository.py
@@ -1,11 +1,21 @@
+from typing import Optional
+from uuid import UUID
+
 from src.core.category.application.category_repository import (
     CategoryRepository,
 )
+from src.core.category.domain.category import Category
 
 
 class InMemoryCategoryRepository(CategoryRepository):
     def __init__(self, categories=None):
         self.categories = categories or []
 
-    def save(self, category):
+    def save(self, category: Category):
         self.categories.append(category)
+
+    def get_by_id(self, id: UUID) ->  Optional[Category]:
+        for category in self.categories:
+            if category.id == id:
+                return category
+        return None
