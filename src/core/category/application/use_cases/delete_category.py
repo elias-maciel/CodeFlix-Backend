0cbb52e8367495a4d82c54931d62080ea364d7c8
elias-maciel <eliasfsilvamaciel@gmail.com>
2025-02-20 18:46:11 -0300

    feat: delete category

---
+++ src/core/category/application/use_cases/delete_category.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from uuid import UUID
+
+from src.core.category.application.use_cases.exceptions import CategoryNotFound
+
+
+@dataclass
+class DeleteCategoryRequest:
+    id: UUID
+
+class DeleteCategory:
+    def __init__(self, repository):
+        self.repository = repository
+
+    def execute(self, request: DeleteCategoryRequest) -> None:
+        category = self.repository.get_by_id(request.id)
+        if category is None:
+            raise CategoryNotFound(f"Category with id {request.id} not found")
+        self.repository.delete(category.id)
