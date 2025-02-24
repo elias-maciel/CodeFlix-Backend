51552696777c07c77d137917daff101b8f83b5c1
elias-maciel <eliasfsilvamaciel@gmail.com>
2025-02-24 16:08:47 -0300

    feat: add retrieve api method

--- src/django_project/category_app/tests/test_category_api.py
+++ src/django_project/category_app/tests/test_category_api.py
@@ -1,31 +1,34 @@
 import pytest
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.test import APIClient
 
 from core.category.domain.category import Category
 from django_project.category_app.repository import DjangoORMCategoryRepository
 
 
+@pytest.fixture
+def category_movie():
+    return Category(
+        name="Movie",
+        description="Movie description",
+    )
+
+
+@pytest.fixture
+def category_documentary():
+    return Category(
+        name="Documentary",
+        description="Documentary description",
+    )
+
+
+@pytest.fixture
+def repository():
+    return DjangoORMCategoryRepository()
+
+
 @pytest.mark.django_db
 class TestCategoryAPI:
-    @pytest.fixture
-    def category_movie(self):
-        return Category(
-            name="Movie",
-            description="Movie description",
-        )
-
-    @pytest.fixture
-    def category_documentary(self):
-        return Category(
-            name="Documentary",
-            description="Documentary description",
-        )
-
-    @pytest.fixture
-    def repository(self):
-        return DjangoORMCategoryRepository()
-
     def test_list_categories(
         self,
         category_movie: Category,
@@ -53,3 +56,32 @@ class TestCategoryAPI:
         assert response.status_code == HTTP_200_OK
         assert len(response.data) == 2
         assert response.data == expected_data
+
+@pytest.mark.django_db
+class TestRetrieveAPI:
+    def test_return_400_when_category_id_is_invalid(self):
+        url = "/api/categories/invalid_id/"
+        response = APIClient().get(url)
+        assert response.status_code == HTTP_400_BAD_REQUEST
+
+    def test_return_category_when_exists(
+        self,
+        category_movie: Category,
+        category_documentary: Category,
+        repository: DjangoORMCategoryRepository,
+    ):
+        repository.save(category_movie)
+        repository.save(category_documentary)
+        url = f"/api/categories/{category_documentary.id}/"
+        response = APIClient().get(url)
+        expected_data = {
+            "id": str(category_documentary.id),
+            "name": category_documentary.name,
+            "description": category_documentary.description,
+            "is_active": category_documentary.is_active,
+        }
+        assert response.status_code == HTTP_200_OK
+        assert response.data == expected_data
+
+    def test_return_404_when_category_not_exists(self):
+        pass
--- src/django_project/category_app/views.py
+++ src/django_project/category_app/views.py
@@ -1,8 +1,11 @@
+from uuid import UUID
+
 from rest_framework import viewsets
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
+from core.category.application.use_cases.get_category import GetCategory, GetCategoryRequest
 from core.category.application.use_cases.list_category import (
     ListCategory,
     ListCategoryRequest,
@@ -29,4 +32,25 @@ class CategoryViewSet(viewsets.ViewSet):
         return Response(
             data=categories,
             status=HTTP_200_OK,
+        )
+
+    def retrieve(self, request: Request, pk=None) -> Response:
+        try:
+            category_pk = UUID(pk)
+        except ValueError:
+            return Response(status=HTTP_400_BAD_REQUEST)
+
+        use_case = GetCategory(repository=DjangoORMCategoryRepository())
+        result = use_case.execute(request=GetCategoryRequest(id=category_pk))
+
+        category_output = {
+            "id": str(result.id),
+            "name": result.name,
+            "description": result.description,
+            "is_active": result.is_active,
+        }
+
+        return Response(
+            status=HTTP_200_OK,
+            data=category_output,
         )
